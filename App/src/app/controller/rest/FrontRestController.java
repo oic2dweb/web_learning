7882cf8f442b3936b8bfd8b55f9a795efdac2412
Amada <Bel@Prog-PC>
2015-10-02 14:53:54 +0900

    CommitTest.kaku

--- App/src/app/controller/rest/FrontRestController.java
+++ App/src/app/controller/rest/FrontRestController.java
@@ -3,10 +3,8 @@ package app.controller.rest;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
 //サーバサイドのAJAX処理設定用クラス
+//test.kaku
 @ApplicationPath("/rest")
 public class FrontRestController extends Application {
 
