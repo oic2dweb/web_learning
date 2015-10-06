b12be8b2e0d47f036d62d6b9ccf35af1b9dbed87
Leo2 <leoncl2000@yahoo.com.hk>
2015-10-06 11:11:36 +0900

    commit

--- App/src/app/controller/WelcomeController.java
+++ App/src/app/controller/WelcomeController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//
 @WebServlet({ "/WelcomeController", "/welcome" })
 public class WelcomeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
